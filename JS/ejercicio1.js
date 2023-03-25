c0e6cf412285a30e1cd8f4f95012b23cdcab3605
camilod29 <cadelgado@uniboyaca.edu.co>
2023-03-24 19:29:14 -0500

    Ejercicio 1 completo
    Ya lo hice jijij

---
+++ JS/ejercicio1.js
@@ -0,0 +1,8 @@
+function pingalin(){
+    var input=document.getElementById('input').value;
+    var largo=input.length;
+    document.getElementById("article_resul").innerHTML = "La cantidad de caracteres es:"+largo;
+}
+
+
+
